0c120b7c7b9d515b53a6c35480fa4f71c9ffb1a0
uyito <uyiaghas@yahoo.com>
2019-10-27 22:51:08 -0500

    t

--- Textify/app/src/main/java/com/example/textify/MainActivity.kt
+++ Textify/app/src/main/java/com/example/textify/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.textify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        val email = email_register.text.toString()
+        val usrname = usrname_register.text.toString()
+        val passwrd = password_register.text.toString()
 
     }
 }
